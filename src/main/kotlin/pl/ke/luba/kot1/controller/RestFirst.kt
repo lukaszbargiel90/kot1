7bb5f7868dbc057f7eb67162b42c948c115a6a9a
lukaszbargiel90 <lukasz-50@wp.pl>
2020-11-05 11:15:56 +0100

    change hello

--- src/main/kotlin/pl/ke/luba/kot1/controller/RestFirst.kt
+++ src/main/kotlin/pl/ke/luba/kot1/controller/RestFirst.kt
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController
 class RestFirst {
 
     @GetMapping("hello")
-    fun sayHello(): String = "hello"
+    fun sayHello(): String = "hello222"
 
 }
